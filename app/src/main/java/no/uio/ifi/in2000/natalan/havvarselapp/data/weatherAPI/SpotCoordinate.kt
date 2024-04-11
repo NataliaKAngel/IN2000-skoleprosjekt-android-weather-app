d70c2aea2c2bf56008af0d9eb7c22908dd64b4ab
hildkva <hildkva@ulrik.uio.no>
2024-04-11 19:58:29 +0200

    Deleted unused class: SpotCoordinate.kt

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/data/weatherAPI/SpotCoordinate.kt
+++
@@ -1,7 +0,0 @@
-package no.uio.ifi.in2000.natalan.havvarselapp.data.weatherAPI
-
-//Class holds the coordinates to each kite spot
-object SpotCoordinate{
-    const val HAMRESANDEN: String = "lat=10&lon=60.1" //OBS: Not the correct coordinate
-}
-
