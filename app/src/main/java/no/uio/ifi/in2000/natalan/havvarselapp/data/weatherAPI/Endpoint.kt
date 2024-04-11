95ea437918ee8ec9d67547b16175e89ee12f17a1
sunnivom <sunnivom@ulrik.uio.no>
2024-04-11 14:57:43 +0200

    Create Endpoint.kt Used Endpoint-object in LocationForecastDataSource.kt Removed TODOS

---
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/data/weatherAPI/Endpoint.kt
@@ -0,0 +1,8 @@
+package no.uio.ifi.in2000.natalan.havvarselapp.data.weatherAPI
+
+object Endpoint {
+    const val LOCATIONFORECAST : String = "https://gw-uio.intark.uh-it.no/in2000/"
+    const val WEATHERRESPONSE : String ="https://api.met.no/weatherapi/locationforecast/2.0/complete?"
+
+
+}
