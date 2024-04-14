2108066dd2c31b79a1a2ddca264961135442a459
hildkva <hildkva@ulrik.uio.no>
2024-04-14 16:41:29 +0200

    Make SpotCard to display information from the Spot-objects

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/MainActivity.kt
@@ -58,7 +58,7 @@ class MainActivity : ComponentActivity() {
 
                     // Creates navController and NavHost
                     val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "HomeScreen") {
+                    NavHost(navController = navController, startDestination = "SpotScreen") {
                      // Navigating routes
                         composable("HomeScreen") { HomeScreen(navController = navController, homeScreenViewModel = homeScreenViewModel) }
                         composable("InfoScreen") { InfoScreen(navController = navController, infoScreenViewModel = infoScreenViewModel) }
--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/spot/SpotScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/spot/SpotScreen.kt
@@ -1,16 +1,20 @@
 package no.uio.ifi.in2000.natalan.havvarselapp.ui.spot
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import no.uio.ifi.in2000.natalan.havvarselapp.model.predefinedSpots.PredefinedSpots
 import no.uio.ifi.in2000.natalan.havvarselapp.model.spot.Spot
 
 @Composable
@@ -45,5 +49,22 @@ fun SpotScreen (
 fun SpotCard(
     spot: Spot?
 ){
-
+    Card{
+        Column {
+            Text("Koordinater: ${spot?.coordinates}")
+            Spacer(modifier = Modifier.height(5.dp))
+            Text("Spotnavn: ${spot?.spotName}")
+            Spacer(modifier = Modifier.height(5.dp))
+            Text("By: ${spot?.cityName}")
+            Spacer(modifier = Modifier.height(5.dp))
+            Text("Vindstyrke: ${spot?.windSpeed}")
+            Spacer(modifier = Modifier.height(5.dp))
+            Text("Vindretning: ${spot?.windDirection}")
+            Spacer(modifier = Modifier.height(5.dp))
+            Text("Enheter: ${spot?.units}")
+            Spacer(modifier = Modifier.height(5.dp))
+            Text("Beregning: ${spot?.recommendationColor}")
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+    }
 }
