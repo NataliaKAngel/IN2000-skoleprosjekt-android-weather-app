06ff31b987e444b556a80708ac4c213731475804
hildkva <hildkva@ulrik.uio.no>
2024-04-23 09:19:20 +0200

    Add instance variable for optimalWindConditions in PredefinedSpots.kt

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/model/predefinedSpots/PredefinedSpots.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/model/predefinedSpots/PredefinedSpots.kt
@@ -3,5 +3,6 @@ package no.uio.ifi.in2000.natalan.havvarselapp.model.predefinedSpots
 data class PredefinedSpots(
     val coordinates: String, //The coordinate of the spot
     val spotName: String, //The name of the spot (example: "Hamresanden")
-    val cityName: String //The city the spot is in (example: "Kristiansand")
+    val cityName: String, //The city the spot is in (example: "Kristiansand")
+    val optimalWindConditions: Map<String, Double> //Min and max value for wind direction
 )
