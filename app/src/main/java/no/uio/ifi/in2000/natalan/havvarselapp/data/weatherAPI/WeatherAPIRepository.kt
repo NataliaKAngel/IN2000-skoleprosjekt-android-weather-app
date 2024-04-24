6f4a5952e237c2113dc1221083e930b2130a9ab5
hildkva <hildkva@ulrik.uio.no>
2024-04-24 20:49:02 +0200

    Implement method transformDate() in WeatherAPIRepository.kt

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/data/weatherAPI/WeatherAPIRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/data/weatherAPI/WeatherAPIRepository.kt
@@ -83,7 +83,24 @@ class WeatherAPIRepository (
     }
 
     private fun transformDate(date: String): String {
-        TODO("Not yet implemented")
+        val (year, month, day) = date.split("-")
+
+        val dateString = day + (when (month){
+            "01" -> ". Januar"
+            "02" -> ". Februar"
+            "03" -> ". Mars"
+            "04" -> ". April"
+            "05" -> ". Mai"
+            "06" -> ". Juni"
+            "07" -> ". Juli"
+            "08" -> ". August"
+            "09" -> ". September"
+            "10" -> ". Oktober"
+            "11" -> ". November"
+            "12" -> ". Desember"
+            else -> ""
+        })
+        return dateString
     }
 
     private fun transformTime(time: String): String {
