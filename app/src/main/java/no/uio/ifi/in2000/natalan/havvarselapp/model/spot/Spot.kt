757cf34277b7fa77695ff0df56ccd564f7cbde7c
ingridearl <ingriefa@uio.no>
2024-04-24 11:00:17 +0200

    changing the instance variables in Spot.kt
    Signed-off-by: ingridearl <ingriefa@uio.no>

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/model/spot/Spot.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/model/spot/Spot.kt
@@ -3,37 +3,11 @@ package no.uio.ifi.in2000.natalan.havvarselapp.model.spot
 import no.uio.ifi.in2000.natalan.havvarselapp.model.locationForecast.WeatherResponse
 import no.uio.ifi.in2000.natalan.havvarselapp.model.metAlerts.Feature
 import no.uio.ifi.in2000.natalan.havvarselapp.model.metAlerts.MetAlertDataClass
+import no.uio.ifi.in2000.natalan.havvarselapp.model.predefinedSpots.PredefinedSpots
 
 data class Spot (
-    //The coordinates of the spot
-    val coordinates: String,
-    //The name of the spot (example: "Hamresanden")
-    val spotName: String,
-    //The city the spot is in (example: "Kristiansand")
-    val cityName: String,
-    //Recommended windDirection for the spot
-    val optimalWindConditions: Map<String, Double>, //Min and max value for wind direction
-    //Photo of the spot as URL
-    val photo: String,
+    val predefinedSpots: PredefinedSpots, // holds the spots coordinate, cityName, spotName and optimal wind condition.
+    val alerts : List<AlertInfo>, // holds a list of all the relevant alerts for the spot (riskMatrixColor, description, event, startTime and endTime)
+    val spotDetails : List<KiteSpotInfo>, // holds a list of the kiting conditions for the spot (some of the variables: date, time, windspeed, winddirection, kiteRecommendationColor)
 
-
-    //LOCATION-FORECAST:
-    //Map containing windSpeed: Map<Time, WindSpeed>
-    val windSpeed : Map<String, Double>,
-    val windSpeedUnit: String?,
-    //Map containing windDirection: Map<Time, WindDirection>
-    val windDirection : Map<String, Double>,
-    val windDirectionUnit: String?,
-
-    //MET-ALERT:
-    //Color of the alert
-    val riskMatrixColor: String?,
-    //Description for the alert
-    val description: String?,
-    //Wind speed level for triggering the alert
-    val triggerLevel: String?,
-
-    //CALCULATION:
-    //Color recommendation for kiting
-    val recommendationColor: String,
 )
