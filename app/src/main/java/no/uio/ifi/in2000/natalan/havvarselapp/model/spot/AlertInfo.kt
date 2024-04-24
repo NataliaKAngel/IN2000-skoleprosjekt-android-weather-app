15565b7e8d25f4631a520e72a4457567327fd110
ingridearl <ingriefa@uio.no>
2024-04-24 10:45:20 +0200

    adding comments to AlertInfo.kt
    Signed-off-by: ingridearl <ingriefa@uio.no>

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/model/spot/AlertInfo.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/model/spot/AlertInfo.kt
@@ -2,10 +2,10 @@ package no.uio.ifi.in2000.natalan.havvarselapp.model.spot
 
 
 data class AlertInfo(
-    val riskMatrixColor : String,
-    val description : String,
-    val event : String,
-    val startTime : String,
-    val endTime : String
+    val riskMatrixColor : String, // Alert color, the overall evaluation from the metAlert API based off the different parameters
+    val description : String, // Detailed description about the alert for the coordinates.
+    val event : String, // The event type e.g. "gale (kuling)"
+    val startTime : String, // the assumed starting time for the alert
+    val endTime : String // the assumed end time for the alert
 )
 
