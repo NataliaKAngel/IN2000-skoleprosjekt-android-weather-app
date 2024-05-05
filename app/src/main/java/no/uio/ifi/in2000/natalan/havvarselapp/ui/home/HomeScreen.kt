999f264ebbace929871ce0e3ee2f69dde0eaa6cf
isabelltran <intran@uio.no>
2024-05-05 22:54:34 +0200

    Adding right windirection icon to buttonsheet and right spot object to button sheet.

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/home/HomeScreen.kt
@@ -33,12 +33,9 @@ import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.annotation.annotations
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotation
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
 import no.uio.ifi.in2000.natalan.havvarselapp.model.spot.Spot
 import no.uio.ifi.in2000.natalan.havvarselapp.ui.components.*
 @OptIn(ExperimentalMaterial3Api::class)
@@ -205,48 +202,6 @@ fun AddAnnotationsToMap(
     }
 }
 
-/*
-@Composable
-fun AddAnnotationsToMap(
-    spots: List<Spot>,
-    context: Context,
-    mapView: MapView,
-    iconId: Map<String, Int>, // Name to icon
-    navController: NavController,
-    homeScreenViewModel: HomeScreenViewModel,
-    clicked : Boolean,
-) {
-    LaunchedEffect(mapView) {
-        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS) { style ->
-            val annotationManager = mapView.annotations.createPointAnnotationManager()
-            spots.forEach { spot ->
-                annotationManager.addClickListener(com.mapbox.maps.plugin.annotation.generated.OnPointAnnotationClickListener {
-                    // Handle click event here
-                    homeScreenViewModel.updateSpotUIState(spot.predefinedSpot.coordinates)
-                    homeScreenViewModel.updateClickedUIState(!clicked)
-                    println("Pin jeg trykker på $spot og ${spot.predefinedSpot.spotName}")
-                    true // Return true if the click event is consumed
-                })
-                // Load your custom icon as a Bitmap
-                val drawable = iconId[spot.predefinedSpot.coordinates]?.let {
-                    AppCompatResources.getDrawable(context, it)
-                }
-                val bitmap = convertDrawableToBitmap(drawable)
-                if (bitmap != null) {
-                    // Add the bitmap as a custom icon in the Mapbox style.
-                    style.addImage(iconId[spot.predefinedSpot.coordinates].toString(), bitmap)
-                    val coordinates = spot.predefinedSpot.coordinates.split(",").map { it.toDouble() }
-                    val point = Point.fromLngLat(coordinates[1], coordinates[0])
-                    val annotationOptions = PointAnnotationOptions()
-                        .withPoint(point)
-                        .withIconImage(iconId[spot.predefinedSpot.coordinates].toString())
-                    annotationManager.create(annotationOptions)
-                }
-            }
-        }
-    }
-}
-*/
 
 // Convert Drawable to Bitmap using this method
 private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
