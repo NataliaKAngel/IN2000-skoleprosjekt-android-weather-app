99132ba1cc2562a12ef6fd125fc98c1150863c3d
sunnivom <sunnivom@ulrik.uio.no>
2024-05-11 23:50:39 +0200

    remove unused imports

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/components/Components.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/components/Components.kt
@@ -12,13 +12,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
 import no.uio.ifi.in2000.natalan.havvarselapp.R
