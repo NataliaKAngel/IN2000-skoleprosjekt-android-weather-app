2f6c93208eb380250de4718b140f01749cfaba1b
sunnivom <sunnivom@ulrik.uio.no>
2024-05-11 08:42:54 +0200

    removed unused imports

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/components/Components.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/ui/components/Components.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
