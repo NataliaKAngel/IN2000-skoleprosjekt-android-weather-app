ffa00bd1483392329e6a580cdc272fe3312a22be
hildkva <hildkva@ulrik.uio.no>
2024-05-12 20:17:56 +0200

    Removed unused imports

--- app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/data/weatherAPI/metAlerts/MetAlertsDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/natalan/havvarselapp/data/weatherAPI/metAlerts/MetAlertsDataSource.kt
@@ -1,7 +1,5 @@
 package no.uio.ifi.in2000.natalan.havvarselapp.data.weatherAPI.metAlerts
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
@@ -10,9 +8,7 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
 import io.ktor.util.appendIfNameAbsent
-import no.uio.ifi.in2000.natalan.havvarselapp.data.weatherAPI.Endpoint.METALERT_CURRENT
 import no.uio.ifi.in2000.natalan.havvarselapp.data.weatherAPI.Endpoint.METALERT_EXAMPLE
-import no.uio.ifi.in2000.natalan.havvarselapp.data.weatherAPI.Endpoint.METALERT_TEST
 import no.uio.ifi.in2000.natalan.havvarselapp.model.metAlerts.MetAlertDataClass
 
 class MetAlertsDataSource {
